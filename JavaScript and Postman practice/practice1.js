75162b162a0755a7b8db12ba0ef43a490f302632
unknown <vladislavdunin.qa@gmail.com>
2022-04-30 16:32:20 +0300

    uprep

---
+++ JavaScript and Postman practice/practice1.js
@@ -0,0 +1,253 @@
+// let number = 15;
+//     if (number == 5 ){
+//             console.log("ok");
+//     }
+//     else if(number != 5){
+//             console.log("maybe");
+//     }else{
+//             console.log("not ok");
+//     }
+
+//     let s1 = "word";
+
+//     switch(s1) {
+//         case "4":
+//             console.l("Peremennaya 1");
+//             break;
+//         case "45":
+//             console.log("peremennaya 2");
+//             break;
+//         case "word1":
+//             console.log("word");
+//             break;
+//         default:
+//             console.log("default");
+//     }
+//    debugger;
+
+// for(let i = 100; i > 5 ; i /= 2){
+//     console.log(i);
+// }
+
+// let s2 = 1000;
+// while (s2 >= 100) {
+//     console.log(s2);
+//     s2-=100;
+// }
+
+// let s3 = true;
+// while (s3) {
+//     console.log(s3);
+// }
+
+
+// let s4 = 100;
+// do {
+//     console.log(s4);
+// }while(s4 < 50);
+
+// for (let i = 10 ; i <=20 ; i +=2){
+//     if(i > 15)
+//     break;
+//     console.log(i);
+// } 
+
+// for(let i = 10; i <=20 ; i++){
+//     if (i % 2 ==0)
+//     continue;
+//     console.log(i);
+// }
+
+// let s5 = [5,7,3,8,91];
+
+// for (let i = 0 ; i < s5.length; i++){
+//     s5[i] *=2;
+
+//     console.log("element " + (i+1) + " : " + s5[i]);
+// }
+
+// function info(word){
+//     console.log("hello " + word);
+//     console.log("!");
+// }
+// info("!");
+
+// function sum (a,b){
+//     let res = a+b;
+//     console.log(res);
+// }
+// sum (5,7);
+
+// function summa (arr) {
+//     let sum = 0;
+//     for(let i = 0;i < arr.length;i++)
+//     sum += arr[i];
+    
+//     return sum;
+// }
+
+// let array = [6,7,8];
+// let res = summa (array);
+// res*=2;
+// console.log("Result "+ res);
+
+// let number1 = 10;
+
+// function info (){
+//     let number1 = 20;
+//     console.log(number1);
+// }
+// info();
+
+// console.log(number1);
+
+
+// let n1 = 10;
+// let n2 = 20;
+// let n3 = 30;
+
+// let my = 18;
+// let my1= 21;
+
+// if (my < n2 || my1 <= n1 ) {
+//     console.log("false");
+// }
+// else if (my >=20 && my1 >=20 ){
+//     console.log("true");   
+// }
+// else if (my >=30 ){
+//     console.log("okey,don't worry");
+// }else{
+//     console.log("goodby");
+// }
+
+// let friends = ["andrey" , "den" , "leha","sanya" , "sophia" ]
+// for (let i = 0 ; i < friends.length ; i++){
+    
+//     console.log("name " +(i+1) +":"+ friends[i]);
+// }
+
+// for (let i = 100; i > 5; i/=2){
+//     console.log(i);
+// }
+
+// function info(hi){
+//     console.log(hi + "Hello");
+// }
+
+// info("hihi");
+
+// function summa (arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     console.log(sum);
+// }
+
+// let array = [1,2,3];
+// let array2= [2,456,55];
+// let array3 = [4,5,66];
+
+// summa(array); 
+// summa(array2);
+// summa(array3);
+
+// function summa (arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//         return sum;
+//     }
+//     console.log(sum);
+// }
+
+// let array = [1,2,3];
+// let array2= [2,456,55];
+// let array3 = [4,5,66];
+
+// summa(array); 
+// // summa(array2);
+// // summa(array3);
+// let res = summa(array);
+// res *=2;
+// console.log(res);
+
+// let names = ["andrey","den","leha","diana","sanya","tanya"];
+// for (let i = 0;i < names.length;i++){
+//     console.log("Name "+(i+1) +":" + names [i]  );
+// }
+
+// const button = document.querySelector("button");
+// const input = document.querySelector(".age");
+// button.onclick = function (){
+//     let num = +input.value;
+//     if (num >= 16 && num < 60){
+//         console.log("welcome");
+//     }
+//     else if (num > 60){
+//         console.log("What?");
+//     }
+//     else if (num > 60){
+//         console.log("What?");
+//     }
+//     else if (num > 60){
+//         console.log("What?");
+    
+    
+//     }else{
+//         console.log("no");
+//     }
+ 
+// switch (num){
+//     case 15:
+//         console.log("wait");
+//         break;
+//     case 16:
+//         console.log("ok");
+//         break;
+//     default:
+//         console.log("ok!!!!!");
+//     } 
+// }
+
+// let b = 5;
+// console.log(b == 3 || b == 7);
+
+
+
+// HW1 --- itgid;
+// const button = document.querySelector("button");
+// const input = document.querySelector(".f1");
+// button.onclick = function(f1){
+//     let i1 = +input.value;
+// if (i1 == 4){
+//     console.log("true");
+// }else{
+//     console.log("don't worry");
+// }
+
+// }
+
+// let a22 = 10;
+// let a21 = 5;
+// const button = document.querySelector("button");
+// const input = document.querySelector(".b2");
+// button.onclick = function(f2){
+
+
+//     if ((out2=a21) > a22){
+//         console.log(a21);  
+//     }
+//     else if (a21 < a22){
+//         console.log(a22);
+//     }else{
+//         console.log("no");
+//     }
+    
+    
+// }
+
+
+
+
